e00054152d709c43c044f70f94592cb91fb75a14
David Haralambiev <114248239+CTSXT@users.noreply.github.com>
2022-12-11 21:48:32 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
+  const elements = document.getElementsByClassName('hot');
+  for(let i = 0 ; i<elements.length;i++){
+    elements[i].insertAdjacentText('beforeend', "🔥")
+  }
   // This block will be executed once the page is loaded and ready
 });
